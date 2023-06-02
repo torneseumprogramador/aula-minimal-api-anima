c79bbb8ee7a78d0871afa6c547af8b2819a9a59b
Eduardo Moraes Goncalves <eduardogoncalves18@gmail.com>
2023-06-02 10:01:35 -0300

    Adição do projeto

--- minimal-api-rota-componente/test/curl/post.sh
+++
@@ -1,8 +0,0 @@
-curl -X 'POST' \
-  'http://localhost:5019/cadastro' \
-  -H 'accept: application/json' \
-  -H 'Content-Type: application/json' \
-  -d '{
-  "item": "teste",
-  "descricao": "tersesss"
-}'
--- minimal-api-rota-unica/Models/Dado.cs
+++
@@ -1,8 +0,0 @@
-namespace Minimal.Models;
-
-record Dado
-{
-    public string? Item { get;set; }
-    public string? Descricao { get;set; }
-}
-
